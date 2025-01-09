e8cbbbdccf5169f69b661d578a18e4a521174c94
Deepali Kundnani <deepali.kundnani@gmail.com>
2025-01-09 18:50:53 -0500

    Update Exp_composition_corr.sh

--- feature_correlations/Exp_composition_corr.sh
+++ feature_correlations/Exp_composition_corr.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+outloc=./poly/
+for file in $(ls *.bed); do
+  cat $file | awk BEGIN'{FS=OFS="\t"}{print $1,$2,$3,$4,$5,$6,substr($4,length($4),1)}' > $outloc/poly_$(basename $file)
+	for N in A C G T; do
+	awk -v n="$N" -F"\t" '{FS=OFS="\t"}{if($7==n) {print $1,$2,$3,$4,$5,$6} }' $outloc/poly_$(basename $file) > $outloc/${N}/$(basename $file)
+	done
+done
 
+file='anno/standardanno/RNAseq/sorted_metadata_500aroundTSS.bed'
+subtypecol=1
+libmeta='/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/Hu_analysis/heatmaps/libmeta_HEK'
+genome='/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/reference/hg38/filtered_hg38-nucleus-noXY.fa.fai'
+
+for nuc in A C G T; do
+bedfiles=$(echo '/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/Hu_analysis/subnfiltbed/nucl/noXY/poly/'${nuc}'/*bed')
+outfolder=$(echo 'locationHM/r'${nuc}'aroundTSS')
+mkdir $outfolder
+bash ~/p-fstorici3-0/rich_project_bio-storici/bin/TAVIR/annotate.sh -r $file -s -c -o $outfolder -b $bedfiles &
+done
