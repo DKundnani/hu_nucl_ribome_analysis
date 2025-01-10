69c1827cce298290b13f5c7ce5e399aba214f92c
Deepali Kundnani <deepali.kundnani@gmail.com>
2025-01-10 12:00:39 -0500

    Update Exp_composition_corr.sh

--- feature_correlations/Exp_composition_corr.sh
+++ feature_correlations/Exp_composition_corr.sh
@@ -12,9 +12,22 @@ subtypecol=1
 libmeta='/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/Hu_analysis/heatmaps/libmeta_HEK'
 genome='/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/reference/hg38/filtered_hg38-nucleus-noXY.fa.fai'
 
+#Each nucleotide separate
 for nuc in A C G T; do
 bedfiles=$(echo '/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/Hu_analysis/subnfiltbed/nucl/noXY/poly/'${nuc}'/*bed')
 outfolder=$(echo 'locationHM/r'${nuc}'aroundTSS')
 mkdir $outfolder
 bash ~/p-fstorici3-0/rich_project_bio-storici/bin/TAVIR/annotate.sh -r $file -s -c -o $outfolder -b $bedfiles &
 done
+
+#All rNMPs
+bedfiles=$(echo '/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/Hu_analysis/subnfiltbed/nucl/noXY/*.bed')
+outfolder=$(echo 'locationHM/raroundTSS')
+mkdir $outfolder
+bash ~/p-fstorici3-0/rich_project_bio-storici/bin/TAVIR/annotate.sh -r $file -s -c -o $outfolder -b $bedfiles &
+
+
+
+Rscript ~/p-fstorici3-0/rich_project_bio-storici/bin/TAVIR/Subtype_ratio.R -a $outfolder/annotated_counts.tsv -c $outfolder/all_counts.tsv -g $genome -f $libmeta -t $subtypecol -o ${outfolder}/$(basename $file)
+#Rscript ~/p-fstorici3-0/rich_project_bio-storici/bin/TAVIR/Subtype_ratio.R -a $outfolder/annotated_counts_same.tsv -c $outfolder/all_counts.tsv -f $libmeta -t $subtypecol  -o ${outfolder}/same_$(basename $file)
+#Rscript ~/p-fstorici3-0/rich_project_bio-storici/bin/TAVIR/Subtype_ratio.R -a $outfolder/annotated_counts_opp.tsv -c $outfolder/all_counts.tsv -f $libmeta -t $subtypecol  -o ${outfolder}/opp_$(basename $file)
